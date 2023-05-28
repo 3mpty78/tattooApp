d232d3ba75997b92b737888848565d992fe7caef
3mpty <aurelien.corre@outlook.fr>
2023-05-28 09:07:19 +0200

    Creating pages

---
+++ pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Chose it,</Text>
+        <Text>Launch it,</Text>
+        <Text>GET IT !</Text>
+      </View>
+    );
+  }
+}
