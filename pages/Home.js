3fe4938432e63c7710bc00b478373bd0ea378195
3mpty <aurelien.corre@outlook.fr>
2023-05-28 09:48:52 +0200

    Adding background to home page

--- pages/Home.js
+++ pages/Home.js
@@ -1,13 +1,23 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, ImageBackground } from "react-native";
+import Styles from "../style/Style";
 
+const image = { uri: "assets/background.jpg" };
 export default class Home extends React.Component {
   render() {
     return (
-      <View>
-        <Text>Chose it,</Text>
-        <Text>Launch it,</Text>
-        <Text>GET IT !</Text>
+      <View style={Styles.view}>
+        <ImageBackground
+          source={require("./assets/background.jpg")}
+          style={Styles.image}
+          resizeMode="cover"
+        >
+          <View style={Styles.slogan}>
+            <Text style={Styles.text}>Chose it,</Text>
+            <Text style={Styles.text}>Launch it,</Text>
+            <Text style={Styles.text}>GET IT !</Text>
+          </View>
+        </ImageBackground>
       </View>
     );
   }
--- style/Style.js
+++ style/Style.js
@@ -1,13 +1,24 @@
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+export default StyleSheet.create({
   view: {
+    flex: 1,
     backgroundColor: "#333",
+    height: "100%",
+  },
+  slogan: {
+    marginTop: "75%",
+    marginLeft: 50,
+    height: "25%",
+  },
+  text: {
+    color: "#fff",
+    flex: 1,
+    flexDirection: "column",
+    fontSize: 30,
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
   },
 });
